af53e7deacf7d082e73ae6ac619acb6e8a624454
GonzaloTafur <gonzatafurbe@gmail.com>
2025-06-03 16:22:57 -0500

    Rediseño de portafolio

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 function imgActual(n, carouselId) {
-    let slides = document.querySelectorAll(`#${carouselId} .imgsld`);
-    slides.forEach(slide => slide.style.display = "none"); 
+    let slides;
+    
+    slides = document.querySelectorAll(`#${carouselId} .imgsld`);
+    
+    // Esto evita que las imagenes seleccionadas se pongan debajo
+    slides.forEach(
+        slide => slide.style.display = "none"
+    );
+    // Esto evita que las imagenes seleccionadas desaparezcan
     slides[n - 1].style.display = "block"; 
 }
-
-document.querySelectorAll("[id^=carrusel]").forEach(carousel => {
-    let slides = carousel.querySelectorAll(".imgsld");
-    slides.forEach((slide, i) => slide.style.display = i === 0 ? "hidden" : "none");
-});
